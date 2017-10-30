5afaf836e786a4392d505f3c34e6ac7f035017fe
Maxim Kotliar <maximuss099@gmail.com>
2017-10-31 00:17:44 +0200

    Initial Commit

---
+++ Observatory/Observatory.h
@@ -0,0 +1,19 @@
+//
+//  Observatory.h
+//  Observatory
+//
+//  Created by Maxim Kotliar on 10/31/17.
+//  Copyright © 2017 Maxim Kotliar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Observatory.
+FOUNDATION_EXPORT double ObservatoryVersionNumber;
+
+//! Project version string for Observatory.
+FOUNDATION_EXPORT const unsigned char ObservatoryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Observatory/PublicHeader.h>
+
+
