fe526e2d42af126d55e496cb7f2e605906b5887f
Maxim Kotliar <maximuss099@gmail.com>
2017-12-15 01:40:03 +0200

    Added UITableView update extension

---
+++ Bindy/Bindy+UIKit.swift
@@ -0,0 +1,38 @@
+//
+//  Bindy+UIKit.swift
+//  Bindy
+//
+//  Created by Maxim Kotliar on 12/15/17.
+//  Copyright © 2017 Maxim Kotliar. All rights reserved.
+//
+
+import UIKit
+
+extension UITableView {
+
+    func perform(updates: [ArrayUpdate],
+                 in section: Int = 0,
+                 insertAnimation: UITableViewRowAnimation = .automatic,
+                 deleteAnimation: UITableViewRowAnimation = .automatic,
+                 replaceAnimation: UITableViewRowAnimation = .automatic) {
+        var insertions: [IndexPath] = []
+        var deletions: [IndexPath] = []
+        var replacements: [IndexPath] = []
+        for update in updates {
+            let indexPaths = update.indexes.map { IndexPath(row: $0, section: section) }
+            switch update.event {
+            case .insert:
+                insertions.append(contentsOf: indexPaths)
+            case .delete:
+                deletions.append(contentsOf: indexPaths)
+            case .replace:
+                replacements.append(contentsOf: indexPaths)
+            }
+        }
+        beginUpdates()
+        insertRows(at: insertions, with: insertAnimation)
+        deleteRows(at: deletions, with: deleteAnimation)
+        reloadRows(at: replacements, with: replaceAnimation)
+        endUpdates()
+    }
+}
