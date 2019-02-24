971b1f00e96114a54b4a3a9f0b35f82571b3c457
Maxim Kotliar <maximuss099@gmail.com>
2019-02-24 21:07:18 +0200

    Added high order functions: map, flatMap, reduce, compactMap, filter (#23)

---
+++ Bindy/HighOrderFunctions.swift
@@ -0,0 +1,81 @@
+//
+//  HighOrderFunctions.swift
+//  Bindy
+//
+//  Created by Maxim Kotliar on 2/24/19.
+//  Copyright © 2019 Maxim Kotliar. All rights reserved.
+//
+
+import Foundation
+
+public protocol BindyOptionalType {
+    associatedtype Wrapped
+    var wrapped: Wrapped? { get }
+}
+
+extension Optional: BindyOptionalType {
+    public var wrapped: Wrapped? {
+        return self
+    }
+}
+
+public extension Observable {
+
+    public func map<U>(_ transform: @escaping (T) -> U) -> Observable<U> {
+        return self.transform(transform)
+    }
+
+    public func map<U: Equatable>(_ transform: @escaping (T) -> U) -> Observable<U> {
+        return self.transform(transform)
+    }
+}
+
+public extension Observable where T: BindyOptionalType {
+
+    public func flatMap<U>(_ transform: @escaping (T) -> U?) -> Signal<U> {
+        let signal = Signal<U>()
+        observe(self) { value in
+            guard let transformed = transform(value) else { return }
+            signal.send(transformed)
+        }
+        return signal
+    }
+}
+
+public extension Observable where T: Collection {
+    public func reduce<Result>(_ initialResult: Result,
+                               nextPartialResult: @escaping (Result, T.Element) -> Result) -> Observable<Result> {
+        let observable = Observable<Result>(initialResult)
+        observe(self) { value in
+            observable.value = value.reduce(initialResult, nextPartialResult)
+        }
+        return observable
+    }
+
+    public func map<Result>(_ transform: @escaping (T.Element) -> Result) -> Observable<[Result]> {
+        let observable = Observable<[Result]>([])
+        observe(self) { value in
+            observable.value = value.map(transform)
+        }
+        return observable
+    }
+
+    public func filter(_ isIncluded: @escaping (T.Element) -> Bool) -> Observable<[T.Element]> {
+         let observable = Observable<[T.Element]>([])
+        observe(self) { value in
+            observable.value = value.filter(isIncluded)
+        }
+        return observable
+    }
+}
+
+public extension Observable where T: Collection, T.Element: BindyOptionalType {
+
+    public func compactMap<Result>(_ transform: @escaping (T.Element?) -> Result?) -> Observable<[Result]> {
+        let observable = Observable<[Result]>([])
+        observe(self) { value in
+            observable.value = value.compactMap(transform)
+        }
+        return observable
+    }
+}
